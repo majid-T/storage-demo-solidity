310977c9dbd2ffc2978bd1c23f303c63949c3c9a
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-21 05:24:40 +0430

    added comments for better understanding

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -17,17 +17,23 @@ contract Storage {
         )
     {
         assembly {
-            // loading all storage data to stack
-            let d1 := sload(bytes4data_slot)
-            let d2 := sload(uintdata_slot)
-            let d3 := sload(booldata_slot)
-            let d4 := sload(addrdata_slot)
+            //First three values are less the 32bytes so will be in slot1, loadinf slot 1 will load all three
+            let dataSlot1 := sload(bytes4data_slot)
 
-            // getting data from stack and bitwise ops where required
-            output1 := shl(224, and(d1, 0xffffffff))
-            output2 := shr(shl(3, uintdata_offset), d1)
-            output3 := d3
-            output4 := d4
+            //Address is 20 bytes so will occupy its own slot, value will be in slot2
+            let dataSlot2 := sload(addrdata_slot)
+
+            // Returning byte4 (4*8 =32 bits first data) - 224+32=256total , thats where 224 is coming from
+            output1 := shl(224, and(dataSlot1, 0xffffffff))
+
+            // Returning uint64 bit value, from its offset in slot1
+            output2 := shr(shl(3, uintdata_offset), dataSlot1)
+
+            // Returning bool value 1 bit, from slot 1
+            output3 := shr(shl(3, booldata_offset), dataSlot1)
+
+            // Returning address which has its own slot2 now
+            output4 := dataSlot2
         }
     }
 }
